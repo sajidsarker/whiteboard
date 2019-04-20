515f1a2db4d981923ec91767a4bc4f8880fdc16f
Student User <studentuser@Students-MacBook-Pro.local>
2019-04-20 00:11:23 -0700

    Updated SQL schema.

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-drop table if exists users, posts;
-create table 'users' {
-	'user_id' bigint unsigned not null autoincrement,
-	'user_username' varchar(20),
-	'user_password' varchar(40),
-	primary key ('user_id')
+drop table if exists users;
+drop table if exists posts;
+create table users(user_id bigint unsigned auto-increment, user_username varchar(20) primary key, user_password varchar(40));
+select * from users;
+create table posts(post_id bigint unsigned auto-increment primary key, user_id varchar(20), post_title varchar(200), post_url varchar(200));
+select * from posts;
