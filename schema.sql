d390b7078adcb3ebb59e788cfa6bce14e736e949
Student User <studentuser@Students-MacBook-Pro.local>
2019-04-25 01:12:40 -0700

    UI/UX Modifications and Functionality

--- schema.sql
+++ schema.sql
@@ -1,6 +1,24 @@
-drop table if exists users;
-drop table if exists posts;
-create table users(user_id bigint unsigned auto-increment, user_username varchar(20) primary key, user_password varchar(40));
-select * from users;
-create table posts(post_id bigint unsigned auto-increment primary key, user_id varchar(20), post_title varchar(200), post_url varchar(200));
-select * from posts;
+DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE users(
+		user_id SERIAL,
+		user_username VARCHAR(20) PRIMARY KEY,
+		user_password VARCHAR(40),
+		user_datetime TIMESTAMP
+	     );
+
+SELECT * FROM users;
+
+CREATE TABLE posts(
+		post_id SERIAL PRIMARY KEY,
+		user_id UNSIGNED BIGINT,
+		user_username VARCHAR(20),
+		post_title MEDIUMTEXT,
+		post_url MEDIUMTEXT,
+		post_body LONGTEXT,
+		post_datetime TIMESTAMP
+	     );
+
+SELECT * FROM posts;
